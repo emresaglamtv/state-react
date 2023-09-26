2fcee8177711510a8f1a7f87cc34222a70c87871
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2023-09-26 12:53:04 +0300

    States-object | React

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [surname, setName] = useState("Emre");
   const [age, setAge] = useState(29);
   const [friends, setFriends] = useState(["Oscar", "Adam"]);
+  const [address, setAddress] = useState({title: "Istanbul", zip: 342345});
 
 
   return (
@@ -17,6 +18,8 @@ function App() {
       <button onClick={() => setAge("25")}>Change Age!</button>
 
       <hr />
+      <br></br>
+
       {
         friends.map((friend, index) => (
           <div key = {index}> {friend} </div>
@@ -25,8 +28,21 @@ function App() {
 
       <br />
 
+      <h2>Friends</h2>
       <button onClick={() => setFriends([...friends, "Eva"])}>
         add new friend
+      </button>
+      
+      <hr />
+      <br></br>
+      
+      <h2>Address</h2>
+      <div>{address.title} {address.zip}</div>
+
+      <br />
+
+      <button onClick={() => setAddress({...address, title: "Ankara", zip: 2342135})}>
+        Change Address!
       </button>
 
     </div> 
